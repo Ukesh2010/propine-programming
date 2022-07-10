e22048dca4a7831bb9d0b43b680aff9fc9630f00
ukesh <ukesh.gaiju@diagonal.software>
2022-07-10 08:51:33 +0545

    USD rate api fetch

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
+const axios = require("axios");
 const args = require("yargs").argv;
+
+const API_KEY = "49eba55a4d05cb02073c57d26df63e232773321deac73dd0e7add57ff422eb4d";
+const URL =
+    "https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH&tsyms=USD&api_key=" + API_KEY;
 
 const {token, date} = args;
 
-if (!token && !date) {
-    console.log("No arguments are provided.")
 
-} else if (token && !date) {
-    console.log("Token is provided.", token)
+(async () => {
+    const response = await axios.get(URL)
+    const useRate = response.data.ETH.USD || 0;
 
-} else if (!token && !date) {
-    console.log("Date is provided.", date)
+    if (!token && !date) {
+        console.log("No arguments are provided.")
 
-} else {
-    console.log("Token and Date both are provided.", {token, date})
+    } else if (token && !date) {
+        console.log("Token is provided.", token)
 
-}
+    } else if (!token && !date) {
+        console.log("Date is provided.", date)
+
+    } else {
+        console.log("Token and Date both are provided.", {token, date})
+
+    }
+})()
+
+
+
+
+
