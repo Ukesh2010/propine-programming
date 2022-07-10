e961ec5e597c73744d4c8b14aaaa47db0f73cde1
ukesh <ukesh.gaiju@diagonal.software>
2022-07-10 13:23:49 +0545

    options to change filepath and token types added

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ const API_KEY =
 const URL =
   "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP&tsyms=USD&api_key=" +
   API_KEY;
-const file = "transactions.csv";
-const TOKEN_TYPES = ["BTC", "ETH", "XRP"];
 
 const args = yargs.argv;
-const { token, date } = args;
+const { token, date, customFilePath, customTokenTypes } = args;
 
-if (token && !TOKEN_TYPES.includes(token)) {
+if (token && !tokenTypes.includes(token)) {
   console.log(
-    "Invalid token type. Available options are " + TOKEN_TYPES.join(",")
+    "Invalid token type. Available options are " + tokenTypes.join(",")
   );
   return;
 }
@@ -28,6 +26,16 @@ if (
   console.log("Invalid date. Please use dd-mm-yyyy format.");
   return;
 }
+
+const filePath = customFilePath || "transactions.csv";
+console.log(`Filepath: ${filePath}`);
+const tokenTypes = (customTokenTypes &&
+  customTokenTypes.split(",").map((tokenType) => tokenType.trim())) || [
+  "BTC",
+  "ETH",
+  "XRP",
+];
+console.log(`Token types: ${tokenTypes.join(",")}`);
 
 const convertEpochToDate = (epoch) => {
   return new Date(epoch * 1000);
@@ -73,15 +81,15 @@ const processTransaction = (row, data, token, date) => {
   }
 };
 
-const getLatestPortfolioValuePerToken = (token, date) => {
+const getPortfolio = (token, date) => {
   return new Promise((resolve, reject) => {
     const data = {};
 
-    TOKEN_TYPES.forEach((types) => {
+    tokenTypes.forEach((types) => {
       data[types] = { amount: 0, timestamp: 0 };
     });
 
-    fs.createReadStream(file)
+    fs.createReadStream(filePath)
       .pipe(fastCSV.parse({ headers: true }))
       .on("error", (error) => {
         reject(error);
@@ -96,67 +104,69 @@ const getLatestPortfolioValuePerToken = (token, date) => {
 };
 
 (async () => {
-  const response = await axios.get(URL);
-  const rates = response.data || 0;
+  try {
+    if (!fs.existsSync(filePath)) {
+      throw new Error("No file found.");
+    }
 
-  if (!token && !date) {
-    console.log("Calculating...");
+    const response = await axios.get(URL);
+    const rates = response.data || 0;
 
-    const data = await getLatestPortfolioValuePerToken();
+    if (!token && !date) {
+      console.log("Calculating...");
 
-    console.log(`Latest portfolio values:\n`);
-    TOKEN_TYPES.forEach((tokenType) => {
+      const data = await getPortfolio();
+
+      console.log(`Latest portfolio values:\n`);
+      tokenTypes.forEach((tokenType) => {
+        console.log(
+          `${tokenType} on ${convertEpochToDate(
+            data[tokenType].timestamp
+          )} : $${getAmountInUSD(
+            data[tokenType].amount,
+            rates[tokenType]?.USD || 0
+          )}\n`
+        );
+      });
+    } else if (token && !date) {
+      console.log("Calculating...");
+
+      const data = await getPortfolio(token);
+      const tokenData = data[token];
+
       console.log(
-        `${tokenType} on ${convertEpochToDate(
-          data[tokenType].timestamp
-        )} : $${getAmountInUSD(
-          data[tokenType].amount,
-          rates[tokenType]?.USD || 0
+        `Latest portfolio value for ${token} on (${convertEpochToDate(
+          tokenData.timestamp
+        )}): $${getAmountInUSD(tokenData.amount, rates[token]?.USD)}\n`
+      );
+    } else if (!token && date) {
+      console.log("Calculating...");
+
+      const data = await getPortfolio(undefined, getEndOfDay(date));
+
+      console.log(`Portfolio values on ${date}:\n`);
+      tokenTypes.forEach((tokenType) => {
+        console.log(
+          `${tokenType} : $${getAmountInUSD(
+            data[tokenType].amount,
+            rates[tokenType]?.USD || 0
+          )}\n`
+        );
+      });
+    } else {
+      console.log("Calculating...");
+
+      const data = await getPortfolio(token, getEndOfDay(date));
+      const tokenData = data[token];
+
+      console.log(
+        `Portfolio value for ${token} on ${date} : $${getAmountInUSD(
+          tokenData.amount,
+          rates[token]?.USD
         )}\n`
       );
-    });
-  } else if (token && !date) {
-    console.log("Calculating...");
-
-    const data = await getLatestPortfolioValuePerToken(token);
-    const tokenData = data[token];
-
-    console.log(
-      `Latest portfolio value for ${token} on (${convertEpochToDate(
-        tokenData.timestamp
-      )}): $${getAmountInUSD(tokenData.amount, rates[token]?.USD)}\n`
-    );
-  } else if (!token && date) {
-    console.log("Calculating...");
-
-    const data = await getLatestPortfolioValuePerToken(
-      undefined,
-      getEndOfDay(date)
-    );
-
-    console.log(`Portfolio values on ${date}:\n`);
-    TOKEN_TYPES.forEach((tokenType) => {
-      console.log(
-        `${tokenType} : $${getAmountInUSD(
-          data[tokenType].amount,
-          rates[tokenType]?.USD || 0
-        )}\n`
-      );
-    });
-  } else {
-    console.log("Calculating...");
-
-    const data = await getLatestPortfolioValuePerToken(
-      token,
-      getEndOfDay(date)
-    );
-    const tokenData = data[token];
-
-    console.log(
-      `Portfolio value for ${token} on ${date} : $${getAmountInUSD(
-        tokenData.amount,
-        rates[token]?.USD
-      )}\n`
-    );
+    }
+  } catch (e) {
+    console.log(e.message);
   }
 })();
